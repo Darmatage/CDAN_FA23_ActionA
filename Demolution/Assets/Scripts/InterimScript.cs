2ebc011efd5af02a78e600615f530fba5f0a8ed8
cthomasdig <cthomasdig@gmail.com>
2023-11-30 14:29:49 -0500

    Door Added

---
+++ Demolution/Assets/Scripts/InterimScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterimScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void toLevel2Funct()
+    {
+
+    }
+    public void toLevel3Funct()
+    {
+
+    }
+    public void toLevel4Funct()
+    {
+
+    }
+    public void toLevel5Funct()
+    {
+
+    }
+}
