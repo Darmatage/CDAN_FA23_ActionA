75ad2364226b773c1546cb3d7984351e62b66ccb
Darmatage <JasonWiserArt@gmail.com>
2023-12-11 21:40:36 -0500

    trees crushable by big player, fish and birds work, enemies flash dark when hit

--- Demolution/Assets/Scripts/Entities_Scripts/CrushableObject.cs
+++ Demolution/Assets/Scripts/Entities_Scripts/CrushableObject.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CrushableObject : MonoBehaviour{
 	
 	public GameObject object_notCrushed;
-	public GameObject object_crushed;
+	public GameObject object_crushed1;
+	public GameObject object_crushed2;
+	public GameObject object_crushed3;
 	
 	public float crushableRange = 2f;
 	private bool isCrushable = false;
@@ -18,15 +20,21 @@ public class CrushableObject : MonoBehaviour{
 	
     void Start(){
 		object_notCrushed.SetActive(true);
-        object_crushed.SetActive(false);
+        object_crushed1.SetActive(false);
+		object_crushed2.SetActive(false);
+		object_crushed3.SetActive(false);
 		//crushVFX.SetActive(false);
 		playerPos = GameObject.FindWithTag("Player").transform;
     }
 
 	void Update(){
 		float distToPlayerFeet = Vector2.Distance(transform.position, playerPos.position);
-		if (distToPlayerFeet <= crushableRange){isCrushable = true;}
-		else {isCrushable = false;}
+		if ((distToPlayerFeet <= crushableRange)&&(GameHandler_PlayerManager.playerLevel < 15)){
+			isCrushable = true;
+		}
+		else {
+			isCrushable = false;
+		}
 		
 		if ((Input.GetAxis("Attack_Stomp") > 0)&&(isCrushable)){
 			CrushMe();
@@ -37,8 +45,12 @@ public class CrushableObject : MonoBehaviour{
 		//crushSFX.Play();
 		//crushVFX.SetActive(true);
 		
+		int crushNum = Random.Range(0,3);
+		if (crushNum ==0){object_crushed1.SetActive(true);}
+		else if (crushNum ==1){object_crushed2.SetActive(true);}
+		else {object_crushed3.SetActive(true);}
+		
         object_notCrushed.SetActive(false);
-		object_crushed.SetActive(true);
 		
 		if (isCar == true){
 			gameObject.GetComponent<Car_Movement>().isCrushed();
--- Demolution/Assets/Scripts/Entities_Scripts/Swim_Fly.cs
+++ Demolution/Assets/Scripts/Entities_Scripts/Swim_Fly.cs
@@ -27,9 +27,7 @@ public class Entity_SwimFly : MonoBehaviour{
     void FixedUpdate(){
 		float distToEnd = Vector2.Distance(creature.transform.position, EndPoint.position);
 		if ((distToEnd >= 0.5f)&&(canMove==true)){
-			LerpAcrossInterval(8f);
-			//Vector2 pos = Vector2.Lerp ((Vector2)StartPoint.position, (Vector2)EndPoint.position, creatureSpeed * Time.time);
-			//creature.transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+			creature.transform.position = Vector2.MoveTowards(creature.transform.position, EndPoint.position, creatureSpeed * Time.deltaTime);
 		} else {
 			creatureArt.SetActive(false);
 			canMove = false;
@@ -43,30 +41,4 @@ public class Entity_SwimFly : MonoBehaviour{
 		creatureArt.SetActive(true);
 		canMove = true;
 	}
-	
-	IEnumerator LerpAcrossInterval(float interval){
-        float fraction = 0.0f;
-        float distance = 0.0f;
-        do
-        {
-            fraction = distance / interval;
-            distance += Time.deltaTime;
- 
-            // Apply Lerp here using 'fraction' as controller alpha, as
-            // fraction goes from 0.0 to 1.0 over time "interval"
- 
-            // for example:
-            Vector2 pos = Vector2.Lerp( (Vector2)StartPoint.position, (Vector2)EndPoint.position, fraction);
-			creature.transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
- 
-            // or alternately:
-            //float size = Mathf.Lerp( minSize, maxSize, fraction);
- 
-            // or for color:
-            //Color color = Color.Lerp( Color.red, Color.green, fraction);
- 
-            yield return null;
-        } while( fraction < 1.0f);
-    }
-	
 }
