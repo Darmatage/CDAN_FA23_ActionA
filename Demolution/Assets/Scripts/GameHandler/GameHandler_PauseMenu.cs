6856d2ecfa344d50630352f0d9ac5cf6036a0155
Darmatage <JasonWiserArt@gmail.com>
2024-01-11 03:05:03 -0500

    Revised level 5 to make it finish-able, and added Attacks reminder screen

--- Demolution/Assets/Scripts/GameHandler/GameHandler_PauseMenu.cs
+++ Demolution/Assets/Scripts/GameHandler/GameHandler_PauseMenu.cs
@@ -6,51 +6,65 @@ using UnityEngine.Audio;
 
 public class GameHandler_PauseMenu : MonoBehaviour {
 
-        public static bool GameisPaused = false;
-        public GameObject pauseMenuUI;
-        public AudioMixer mixer;
-        public static float volumeLevel = 1.0f;
-        private Slider sliderVolumeCtrl;
+	public static bool GameisPaused = false;
+	public GameObject pauseMenuUI;
+	public AudioMixer mixer;
+	public static float volumeLevel = 1.0f;
+	private Slider sliderVolumeCtrl;
 
-        void Awake (){
-                SetLevel (volumeLevel);
-                GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
-                if (sliderTemp != null){
-                        sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
-                        sliderVolumeCtrl.value = volumeLevel;
-                }
-        }
 
-        void Start (){
-                pauseMenuUI.SetActive(false);
-                GameisPaused = false;
-        }
+	public GameObject attacksMenu;
+	
+	void Awake (){
+		SetLevel (volumeLevel);
+		GameObject sliderTemp = GameObject.FindWithTag("PauseMenuSlider");
+		if (sliderTemp != null){
+			sliderVolumeCtrl = sliderTemp.GetComponent<Slider>();
+			sliderVolumeCtrl.value = volumeLevel;
+		}
+	}
 
-        void Update (){
-                if (Input.GetKeyDown(KeyCode.Escape)){
-                        if (GameisPaused){
-                                Resume();
-                        }
-                        else{
-                                Pause();
-                        }
-                }
-        }
+	void Start (){
+		attacksMenu.SetActive(false);
+		pauseMenuUI.SetActive(false);
+		GameisPaused = false;
+	}
 
-        public void Pause(){
-                pauseMenuUI.SetActive(true);
-                Time.timeScale = 0f;
-                GameisPaused = true;
-        }
+	void Update (){
+		if (Input.GetKeyDown(KeyCode.Escape)){
+			if (GameisPaused){
+				Resume();
+			}
+			else{
+				Pause();
+			}
+		}
+	}
 
-        public void Resume(){
-                pauseMenuUI.SetActive(false);
-                Time.timeScale = 1f;
-                GameisPaused = false;
-        }
+	public void Pause(){
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameisPaused = true;
+	}
 
-        public void SetLevel (float sliderValue){
-                mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
-                volumeLevel = sliderValue;
-        }
+	public void Resume(){
+		attacksMenu.SetActive(false);
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameisPaused = false;
+	}
+
+	public void SetLevel (float sliderValue){
+		mixer.SetFloat("MusicVolume", Mathf.Log10 (sliderValue) * 20);
+		volumeLevel = sliderValue;
+	}
+		
+	public void OpenAttacksMenu(){
+		attacksMenu.SetActive(true);
+	}
+	public void closeAttacksMenu(){
+		attacksMenu.SetActive(false);
+	}
+		
+		
 }
